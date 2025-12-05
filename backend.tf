853dec728384b67a3341caba03cfb98e939f8380
sundaytj <125780223+sundaytj@users.noreply.github.com>
2025-12-05 11:11:24 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "tooling/terraform.state"
-    bucket         = "class49-terraform-backend-bucket"
+    bucket         = "class31-terraform-backend-bucket"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
